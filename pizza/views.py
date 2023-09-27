47ae2442d49e2ac6b4e746fed238317fa0536cd8
OlegatorLE <oleh.travin@gmail.com>
2023-09-27 22:10:17 +0300

    Add PizzaListView in views

--- pizza/views.py
+++ pizza/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 
+from pizza.models import Pizza
 
-class HomePageView(generic.ListView):
-    pass
+
+class PizzaListView(generic.ListView):
+    model = Pizza
+    template_name = "pizza/index.html"
+
 
