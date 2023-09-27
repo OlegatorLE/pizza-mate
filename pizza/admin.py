330109a662d2dfea30f1825fbf6b7a98db97e3ff
OlegatorLE <oleh.travin@gmail.com>
2023-09-27 20:59:35 +0300

    Add CustomUser, Pizza, PizzaSize to admin.py & migrate

--- pizza/admin.py
+++ pizza/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from pizza.models import CustomUser, Pizza, PizzaSize
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("phone", "address",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional Info", {"fields": ("phone", "address",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional Info",
+            {
+                "fields": ("first_name", "last_name", "phone", "address",)
+            }
+        )
+    )
+
+
+@admin.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ("name", "base_price", "size", "user")
+
+
+@admin.register(PizzaSize)
+class PizzaSizeAdmin(admin.ModelAdmin):
+    list_display = ("name", "multiplier",)
