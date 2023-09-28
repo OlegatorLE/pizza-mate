6bd91c4c89e584417353c105efec1364acb58351
OlegatorLE <oleh.travin@gmail.com>
2023-09-28 20:59:13 +0300

    added the ability to change the list of ingredients in the pizza. fixed some problems in templates

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from pizza.views import PizzaListView, CustomUserCreateView
+from pizza.views import (
+    PizzaListView,
+    CustomUserCreateView,
+    PizzaDetailView,
+    show_updated_pizza,
+)
 
 urlpatterns = [
     path("", PizzaListView.as_view(), name="home-page"),
     path("users/create/", CustomUserCreateView.as_view(), name="customuser-create"),
+    path("detail/<int:pk>/", PizzaDetailView.as_view(), name="pizza-detail"),
+    path('pizza/<int:pk>/update_ingredients/',
+         show_updated_pizza, name='update-pizza-ingredients'),
 ]
 
 app_name = "pizza"
--- pizza/views.py
+++ pizza/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
 from pizza.forms import CustomUserCreationForm
-from pizza.models import Pizza, CustomUser
+from pizza.models import Pizza, CustomUser, Ingredient
 
 
 class PizzaListView(generic.ListView):
@@ -16,3 +16,50 @@ class CustomUserCreateView(generic.CreateView):
     model = CustomUser
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
+
+
+class PizzaDetailView(generic.DetailView):
+    model = Pizza
+
+    def post(self, request, *args, **kwargs):
+        size = request.POST.get('size', 'small')
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object, size=size)
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['size'] = kwargs.get('size', 'small')
+        all_ingredients = Ingredient.objects.all()
+        half_length = len(all_ingredients) // 2
+        context['left_ingredients'] = all_ingredients[:half_length]
+        context['right_ingredients'] = all_ingredients[half_length:]
+        return context
+
+
+def show_updated_pizza(request, *args, **kwargs):
+    pizza = get_object_or_404(Pizza, id=kwargs["pk"])
+
+    selected_ingredients = request.POST.getlist("ingredients")
+    ingredient_quantities = {}
+    for ingredient_id in selected_ingredients:
+        qty_key = f"ingredient_qty_{ingredient_id}"
+        ingredient_quantities[ingredient_id] = request.POST.get(qty_key, 1)
+
+    updated_ingredients = []
+    for ingredient_id, quantity in ingredient_quantities.items():
+        ingredient = get_object_or_404(Ingredient, id=ingredient_id)
+        updated_ingredients.append({
+            "ingredient": ingredient,
+            "quantity": quantity,
+        })
+
+    pizza_size = request.POST.get("size")
+
+    context = {
+        "pizza": pizza,
+        "updated_ingredients": updated_ingredients,
+        "pizza_size": pizza_size,
+    }
+
+    return render(request, 'pizza/update_pizza_detail.html', context)
