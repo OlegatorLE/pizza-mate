b9ce13eaf7466c1af24ac5eb7bb2896926817f03
OlegatorLE <oleh.travin@gmail.com>
2023-09-30 14:23:02 +0300

    Added the ability to order basic and custom pizzas. Each user can view the history of their orders

--- pizza/urls.py
+++ pizza/urls.py
@@ -5,6 +5,9 @@ from pizza.views import (
     CustomUserCreateView,
     PizzaDetailView,
     show_updated_pizza,
+    OrderPizzaView,
+    OrderConfirmationView,
+    UserOrdersView,
 )
 
 urlpatterns = [
@@ -13,6 +16,9 @@ urlpatterns = [
     path("detail/<int:pk>/", PizzaDetailView.as_view(), name="pizza-detail"),
     path('pizza/<int:pk>/update_ingredients/',
          show_updated_pizza, name='update-pizza-ingredients'),
+    path('order/<int:pk>/', OrderPizzaView.as_view(), name='order-pizza'),
+    path('order-confirmation/', OrderConfirmationView.as_view(), name='order-confirmation'),
+    path('my-orders/', UserOrdersView.as_view(), name='user-orders'),
 ]
 
 app_name = "pizza"
--- pizza/views.py
+++ pizza/views.py
@@ -1,10 +1,13 @@
+from copy import copy
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
 from pizza.forms import CustomUserCreationForm
-from pizza.models import Pizza, CustomUser, Ingredient
+from pizza.models import Pizza, CustomUser, Ingredient, IngredientType, Order, \
+    OrderPizza
 
 
 class PizzaListView(generic.ListView):
@@ -24,23 +27,32 @@ class PizzaDetailView(generic.DetailView):
     def post(self, request, *args, **kwargs):
         size = request.POST.get('size', 'small')
         self.object = self.get_object()
-        context = self.get_context_data(object=self.object, size=size)
+        pizza = get_object_or_404(Pizza, id=kwargs["pk"])
+        pizza_prices = pizza.get_prices()
+        print(pizza_prices)
+        context = self.get_context_data(object=self.object, size=size, pizza_prices=pizza_prices)
         return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['size'] = kwargs.get('size', 'small')
-        context["sauce"] = Ingredient.objects.filter(ingredient_type__name="Sauces")
-        context["cheese"] = Ingredient.objects.filter(ingredient_type__name="Cheese")
-        context["meat"] = Ingredient.objects.filter(ingredient_type__name="Meat")
-        context["fruit"] = Ingredient.objects.filter(ingredient_type__name="Fruits")
-        context["vegetable"] = Ingredient.objects.filter(ingredient_type__name="Vegetables")
-        context["edge"] = Ingredient.objects.filter(ingredient_type__name="Сheese edge")
+        context["pizza_price"] = kwargs.get("pizza_prices")
+        ingredient_types = IngredientType.objects.all().prefetch_related(
+            'ingredients'
+        )
+
+        for ingredient_type in ingredient_types:
+            context_key = ingredient_type.name.lower().replace(" ", "_")
+            context[context_key] = ingredient_type.ingredients.all()
+
         return context
 
 
 def show_updated_pizza(request, *args, **kwargs):
     pizza = get_object_or_404(Pizza, id=kwargs["pk"])
+    pizza_prices = pizza.get_prices()
+
+    base_ingredients_quantity = len([ing.name for ing in pizza.ingredients.all()])
 
     selected_ingredients = request.POST.getlist("ingredients")
     ingredient_quantities = {}
@@ -58,10 +70,71 @@ def show_updated_pizza(request, *args, **kwargs):
 
     pizza_size = request.POST.get("size")
 
+    updated_ingredients_quantity = sum(
+        int(ingredient["quantity"]) for ingredient in updated_ingredients
+    )
+
+    copy_pizza_prices = copy(pizza_prices)
+    price_difference = updated_ingredients_quantity - base_ingredients_quantity
+    if price_difference != 0:
+        for key, pizza_price in copy_pizza_prices.items():
+            pizza_price += price_difference * 10
+            pizza_prices[key] = pizza_price
+
     context = {
         "pizza": pizza,
         "updated_ingredients": updated_ingredients,
         "pizza_size": pizza_size,
+        "pizza_prices": pizza_prices,
     }
 
     return render(request, 'pizza/update_pizza_detail.html', context)
+
+
+class OrderPizzaView(LoginRequiredMixin, generic.DetailView):
+    model = Pizza
+    template_name = "pizza/order_pizza.html"
+
+    def post(self, request, *args, **kwargs):
+        pizza = self.get_object()
+        pizza_price = self.request.POST.get("pizza_price")
+        request.session["pizza_price"] = pizza_price
+        return redirect("pizza:order-pizza", pk=pizza.id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["pizza_price"] = self.request.session.get("pizza_price")
+        return context
+
+
+class OrderConfirmationView(LoginRequiredMixin, generic.View):
+    model = Order
+    template_name = "pizza/order_confirmation.html"
+
+    def post(self, request, *args, **kwargs):
+        pizza_id = request.POST.get("pizza")
+        pizza = get_object_or_404(Pizza, id=pizza_id)
+
+        quantity = int(request.POST.get("quantity"))
+        pizza_price = float(request.POST.get("pizza_price"))
+
+        print(pizza, quantity, pizza_price)
+        order = Order.objects.create(
+            customer=request.user,
+            total_price=pizza_price * quantity
+        )
+        OrderPizza.objects.create(
+            order=order,
+            pizza=pizza,
+            quantity=quantity
+        )
+
+        return redirect("pizza:home-page")
+
+
+class UserOrdersView(LoginRequiredMixin, generic.ListView):
+    model = Order
+    template_name = 'pizza/user_orders.html'
+
+    def get_queryset(self):
+        return Order.objects.filter(customer=self.request.user)
