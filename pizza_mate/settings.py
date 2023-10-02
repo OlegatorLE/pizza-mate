09143f8494a3c2dbd388bb8f5c66596a336693c8
OlegatorLE <oleh.travin@gmail.com>
2023-10-02 12:01:19 +0300

    Removed insecure django secret key from settings

--- pizza_mate/settings.py
+++ pizza_mate/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-104s_%osgo55b-+592&^*ko=7!v*=7xiq*ewiloo0l0w5ih8t!")  # noqa: E501
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
+
 
 # Application definition
 
